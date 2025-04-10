9c77ed7a7cdb5cb46440f807feb8b7aaaa99f2f6
mendexco <vitormendesco.js@gmail.com>
2025-04-10 18:47:50 -0300

    feat(ui): aligning joystick to bottom and using default directions inputs

--- src/scripts/helpers/input_helpers.gd
+++ src/scripts/helpers/input_helpers.gd
@@ -2,4 +2,4 @@ class_name InputHelpers
 
 
 static func get_movement_input_vector() -> Vector2:
-	return Input.get_vector("move_left", "move_right", "move_up", "move_down")
+	return Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
